0c9a03a612ff6fa4ecb8af849ca7c23ad65713ec
Super Liu <supercjliu@google.com>
2019-10-16 19:46:24 +0800

    include: linux: add CONFIG_DRM to include correct msm drm notify API
    Because commit a6869840b02a ("drm/msm: cleanup display drivers to
    restore upstream files") move display driver from `drivers/gpu/drm/msm`
    to `techpack/display/msm`, it unset CONFIG_MSM_DRM by defconfig
    and include related definitions in `techpack/display/config` to build
    corresponding APIs. This will be tricky for msm_drm_notify.h without
    CONFIG_MSM_DRM in msm-kernel project. Therefore, add CONFIG_DRM
    as alternative to resolve this.
    
    Bug: 142832271
    Change-Id: I4ea5a3a8295a1c4b78675cb6f9eaa87e0474182e
    Merged-In: I4ea5a3a8295a1c4b78675cb6f9eaa87e0474182e
    Signed-off-by: Super Liu <supercjliu@google.com>
    (cherry picked from commit bbc7c1a3f6c26005e1b512311b17d4921b89d2d3)

--- include/linux/msm_drm_notify.h
+++ include/linux/msm_drm_notify.h
@@ -52,7 +52,7 @@ struct msm_drm_notifier {
 
 int dsi_panel_backlight_get(void);
 
-#ifdef CONFIG_DRM_MSM
+#if defined(CONFIG_DRM_MSM) || defined(CONFIG_DRM)
 int msm_drm_register_client(struct notifier_block *nb);
 int msm_drm_unregister_client(struct notifier_block *nb);
 int msm_drm_notifier_call_chain(unsigned long val, void *v);
